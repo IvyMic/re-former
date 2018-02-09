81dd8df08ee3306f0b82dc1e0ada69ec368ed875
IvyMic <ibrahim_e@hotmail.co.uk>
2018-02-09 20:02:21 +0000

    fixed routes and controller. added update action and modified edit action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,10 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to edit_user_path(@user)
     else
